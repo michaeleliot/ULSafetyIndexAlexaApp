297fa8aa655990860271ae8dae46fc318c941914
Michael Eliot <michael.eliot@aol.com>
2017-01-08 11:45:39 -0800

    started making changes for fact

--- src/index.js
+++ src/index.js
@@ -112,6 +112,20 @@ var handlers = {
             this.emit(':ask', speechOutput, repromptSpeech);
         }
     },
+    'GetNewFactIntent': function () {
+        this.emit('GetFact');
+    },
+    'GetFact': function () {
+        // Get a random space fact from the space facts list
+        // Use this.t() to get corresponding language data
+        var factArr = this.t('FACTS');
+        var factIndex = Math.floor(Math.random() * factArr.length);
+        var randomFact = factArr[factIndex];
+
+        // Create speech output
+        var speechOutput = this.t("GET_FACT_MESSAGE") + randomFact;
+        this.emit(':tellWithCard', speechOutput, this.t("SKILL_NAME"), randomFact)
+    },
     'WhoUseIntent' : function() {
         this.emit(':tell', this.t("WHOW_USE"))
     },
@@ -172,6 +186,22 @@ var languageStrings = {
             HOW_MANY: "Nearly one hundred ninety countries are represented in the UL Safety Index database.",
             MAXIMUM: "The country with the highest safety index is the Netherlands with a rating of 95.39.",
             MINIMUM: "The country with the lowest safety index is South Sudan with a rating of 21.46.",
+            GET_FACT_MESSAGE : "Here's how you can help: ",
+            FACTS : [
+                "A year on Mercury is just 88 days long.",
+                "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
+                "Venus rotates anti-clockwise, possibly because of a collision in the past with an asteroid.",
+                "On Mars, the Sun appears about half the size as it does on Earth.",
+                "Earth is the only planet not named after a god.",
+                "Jupiter has the shortest day of all the planets.",
+                "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
+                "The Sun contains 99.86% of the mass in the Solar System.",
+                "The Sun is an almost perfect sphere.",
+                "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
+                "Saturn radiates two and a half times more energy into space than it receives from the sun.",
+                "The temperature inside the Sun can reach 15 million degrees Celsius.",
+                "The Moon is moving approximately 3.8 cm away from our planet every year."
+            ],
         },
     },
 };
